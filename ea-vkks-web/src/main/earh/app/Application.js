ea7716c81b3baaf53771e1e11776c0c2a02bdd55
Serge Blagodatskih <blagodatskih@insoft.ru>
2014-12-01 12:31:16 +0300

    error

--- ea-vkks-web/src/main/earh/app/Application.js
+++ ea-vkks-web/src/main/earh/app/Application.js
@@ -13,26 +13,24 @@ Ext.define('Earh.Application', {
 	],
 	name: 'Earh',
 	launch: function () {
-		Earh.user = "Иванов И. И.";
-		Earh.editRole = true;
-		Ext.create('widget.eamain');
-		/*
-		 Ext.Ajax.request({
-		 url: Urls.user,
-		 success: function (answer) {
-		 var result = Ext.decode(answer.responseText),
-		 roles = Earh.roles = result.roles;
-		 Earh.user = result.user;
+//		Earh.user = "Иванов И. И.";
+//		Earh.editRole = true;
+//		Ext.create('widget.eamain');
+		Ext.Ajax.request({
+			url: Urls.user,
+			success: function (answer) {
+				var result = Ext.decode(answer.responseText),
+						roles = Earh.roles = result.roles;
+				Earh.user = result.user;
 
-		 Earh.editRole = !!~roles.indexOf('CODE_OF_EDIT_ROLE');
-		 if (Earh.editRole || ~roles.indexOf('CODE_OF_VIEW_ROLE'))
-		 Ext.create('widget.eamain');
-		 else {
-		 showError("Ошибка", "Пользователь " + Earh.user + " не имеет прав для доступа к приложению");
-		 window.location.href = Urls.logout;
-		 }
-		 }
-		 });
-		 */
+				Earh.editRole = !!~roles.indexOf('CODE_OF_EDIT_ROLE');
+				if (Earh.editRole || ~roles.indexOf('CODE_OF_VIEW_ROLE'))
+					Ext.create('widget.eamain');
+				else {
+					showError("Ошибка", "Пользователь " + Earh.user + " не имеет прав для доступа к приложению");
+					window.location.href = Urls.logout;
+				}
+			}
+		});
 	}
 });
--- ea-vkks-web/src/main/earh/app/model/Tree.js
+++ ea-vkks-web/src/main/earh/app/model/Tree.js
@@ -1,5 +1,8 @@
+/**
+ * Модель для иерархических справочников
+ */
 Ext.define('Earh.model.Tree', {
 	extend: 'Ext.data.Model',
-	fields: ['id', 'name']
+	fields: ['id', 'path', 'text']
 });
 
